a9be415e72aa6330ea02798fdd442f59ae0ba8b0
Rachel <marcotter25@yahoo.com>
2022-11-23 12:44:04 -0800

    added photo gallery

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import About from './components/About'
 import Nav from './components/Nav'
+import Gallery from "./components/Gallery"
 
 function App() {
-  return (
+  const [categories] = useState([
+    { name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects' },
+    { name: 'portraits', desctiption: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, and the beauty of nature' }
+]);
+
+const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  
+return (
     <div>
-      <Nav />
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
       <main>
+        <Gallery />
         <About />
       </main>
          
